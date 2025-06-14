a8f6c9127695b39eee0a9ce927ae7861cac4da38
heojawon <ljhmm10@gmail.com>
2025-06-14 18:07:28 +0900

    publish settings 수정4

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,22 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/heojawon/",
 	server: {
 		watch: {
 			usePolling: true,
 		},
 		hmr: {
 			overlay: true,
+		},
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				assetFileNames: "assets/[name].[ext]",
+				chunkFileNames: "assets/[name].[hash].js",
+				entryFileNames: "assets/[name].[hash].js",
+			},
 		},
 	},
 });
